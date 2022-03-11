db73dbbebe392fb30f91a6a56c420db6817a14c6
DenilDenilson <jordanpreseleccion@gmail.com>
2022-03-11 00:17:20 -0500

    Mnnnn cositas, aún intentando averiguar el tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,44 @@
+// //const defaultTheme = require('tailwindcss/defaultTheme');
+
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     screens: {
+//       'xl': '1440px',
+//     },
+
+//     extend: {
+//       colors: {
+//         'Soft-red': '#FE7866',
+//         'Yellow': '#F9D400',
+//         'Dark-desaturated-cyan': '#24554B',
+//         'Dark-blue': '#19526B',
+//         'Dark-moderate-cyan': '#458C7D',
+  
+//         'Very-dark-desaturated-blue': '#232F3D',
+//         'Very-dark-grayish-blue': '#5A626C',
+//         'Dark-grayish-blue': '#808397',
+//         'Grayish-blue': '#A7AAAE',
+//         'White': '#FFFFFF',
+//       },
+//       // fontFamily: {
+//       //   sans: [
+//       //     'Lato',
+//       //     ...defaultTheme.fontFamily.sans,
+//       //   ]
+//       // }
+//       fontFamily: {
+//         Barlow: ['Barlow', 'sans-serif'],
+//         Fraunces: ['Fraunces', 'serif'],
+//       },
+//       backgroundImage: {
+//         'image-header': "url('./images/mobile/image-header.jpg')",
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
